c0a0d0c357172645d425857e757eca65033b11ae
Anique <aniquedavla@gmail.com>
2017-03-07 12:09:20 -0800

    binary search iterative

--- src/TestAlgos.java
+++ src/TestAlgos.java
@@ -28,6 +28,18 @@ public class TestAlgos {
     }
 
     public boolean binarySearchIterative(int[] array, int value){
-        int i=0
+        int low = 0;
+        int high =  array.length - 1;
+        while(low < high){
+            int mid = (high - low)/2;
+            if(value == array[mid]){
+                return true;
+            } else if(value < mid){
+                high = mid -1;
+            } else if(value > mid){
+                low = mid +1;
+            }
+        }
+        return false;
     }
 }
