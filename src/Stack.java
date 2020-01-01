6711bae80ecfd204eeb615addcbbf27b3dd5534f
Anique Davla <aniquedavla@gmail.com>
2020-01-01 10:33:51 -0800

    data structures

--- src/Stack.java
+++ src/Stack.java
@@ -1,13 +1,27 @@
 /**
  * Created by aniquedavla on 3/7/17.
  */
+
+//array implementation of a Stack
 public class Stack {
-    Node head;
-    public Stack(){
+
+    int lastIndex;
+    int firstIndex;
+    int[] stackArray;
+
+    public Stack(int size){
+        stackArray = new int[size];
+    }
+
+    public class push(int data){
+        stackArray[]
+    }
+}
+
+    public class Stack {
+        public Stack(){
 
     }
 
-    public class Node{
-        Node next;
-    }
+
 }
